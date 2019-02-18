cce8b073dd8bd87f9321fcb3ac37d1b4c90df2ba
dogancankilment <dogancankilment@gmail.com>
2019-02-18 06:00:31 -0800

    base django manage.py

---
+++ lpbusiness/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^$',
+                           'lpbusiness.views.home',
+                           name='home'),
+                       )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$',
+                             'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+
+if settings.DEBUG:   # if DEBUG is True it will be served automatically
+    urlpatterns += patterns('',
+                            url(r'^static/(?P<path>.*)$',
+                                'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+                            )
