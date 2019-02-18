35dea3d8fcbf2bcea01d4434771f54784d817c33
dogancankilment <dogancankilment@gmail.com>
2019-02-18 06:00:55 -0800

    lpbusiness django project started

---
+++ home/models.py
@@ -0,0 +1,70 @@
+"""
+    LpBusiness Database Design
+
+
+    Vendor_Account_Manager
+    -
+    id pk int
+    title string
+    mail string
+    phone string
+    partner int FK >- Partner_Contact.id
+    customer FK >- Customer_Contact.id int
+
+    Partner_Contact
+    -
+    id pk int
+    title string
+    mail string
+    phone string
+    customer int FK >- Customer_Contact.id
+    vendor int FK >- Vendor_Account_Manager.id
+
+    Customer_Contact
+    -
+    id pk int
+    title string
+    mail string
+    phone string
+
+    Project
+    -
+    id pk int
+    title string
+    product string
+    date string
+    notes string
+    extra string
+    status boolean
+
+"""
+from django.db import models
+
+
+class Customer(models.Model):
+    title = models.CharField(max_length=100)
+    mail = models.EmailField()
+    phone = models.CharField(max_length=100)
+
+
+class Partner(models.Model):
+    title = models.CharField(max_length=100)
+    mail = models.EmailField()
+    phone = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer)
+
+
+class Vendor(models.Model):
+    title = models.CharField(max_length=100)
+    mail = models.EmailField()
+    phone = models.CharField(max_length=100)
+    customer = models.ForeignKey(Customer)
+    partner = models.ForeignKey(Partner)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    product = models.CharField(max_length=100)
+    information = models.CharField(max_length=100)
+    notes = models.TextField()
+    status = models.BooleanField(default=False)
