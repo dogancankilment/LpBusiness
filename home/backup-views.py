d1722a4dc396a20eaa1dc4d56b94c555ca173be2
dogancankilment <dogancankilment@gmail.com>
2019-03-27 17:44:24 +0300

    views backup

---
+++ home/backup-views.py
@@ -0,0 +1,11 @@
+# all_customers = Customer.objects.all()
+# all_vendors = Vendor.objects.all()
+# all_partners = Partner.objects.all()
+# len_customers = len(all_customers)
+# len_vendors = len(all_vendors)
+# len_partners = len(all_partners)
+#
+# t = {"request": request,
+# "len_customers": len_customers,
+# "len_partners": len_partners,
+# "len_vendors": len_vendors}
